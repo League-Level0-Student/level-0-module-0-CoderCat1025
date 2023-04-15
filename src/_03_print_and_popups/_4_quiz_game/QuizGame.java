7cf4267385ff3475bc16220d5618a110fc953f0b
league <league@imac-32.attlocal.net>
2023-04-14 18:24:26 -0700

    More coding.

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,23 +1,31 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
-		
+
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
-		
-				// 2.  Ask the user a question 
-				
-				// 3.  Use an if statement to check if their answer is correct
-				
-				// 4.  if the user's answer was correct, add one to their score 
-		
+
+		// 2.  Ask the user a question 
+		String prompt = "What color is the sky?";
+		String answer = JOptionPane.showInputDialog(prompt);
+		// 3.  Use an if statement to check if their answer is correct
+		if (answer.equals ("Blue")) {
+			// 4.  if the user's answer was correct, add one to their score 
+			score+=1;
+		}
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
-				// Option: Subtract a point from their score for a wrong answer
-		
-		
+		// Option: Subtract a point from their score for a wrong answer
+
+
+		String prompt2 = "What is floccinaucinihilipilification?";
+		String answer2 = JOptionPane.showInputDialog(prompt2);
+		if (answer2.equals("The estimation of something or someone as useless")); {
+			score+=1;}
 		// After all the questions have been asked, tell the user their final score 
-		
+ JOptionPane.showMessageDialog(null, "Your score was " +score+ " out of 2.");
 	}
 }
--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -2,35 +2,40 @@ package _03_print_and_popups._5_awesome_or_not;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes everything below
+	public static void main(String [] args) {
 
-	
 		Random ran = new Random();    //This will be used below to make a random number. 
-		
-		// 2. Make a variable that will hold a random whole number
-	
-		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(4); 
-	
-		// 3. Print your variable to the console
-	
-		// 4. Get the user to enter something that they think is awesome
-	
-		// 5. If your variable is  0
-	
-			// -- tell the user whatever they entered is awesome!
-	
-		// 6. If your variable is  1
-	
-			// -- tell the user whatever they entered is ok.
-	
-		// 7. If your variable is  2
-	
-			// -- tell the user whatever they entered is boring.
-	
-		// 8. If your variable is  3
-	
-			// -- invent your own message to give to the user (be nice).
 
-}
+		// 2. Make a variable that will hold a random whole number
+		int randomNumber=ran.nextInt(4);
+
+		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(4); 
+
+		// 3. Print your variable to the console
+		System.out.println(randomNumber);
+		// 4. Get the user to enter something that they think is awesome
+		String prompt = "Enter something awesome.";
+		// 5. If your variable is  0
+		if (randomNumber==0) {
+			// -- tell the user whatever they entered is awesome!
+			JOptionPane.showMessageDialog (null, "Wow, that was awesome."); 
+		}
+		// 6. If your variable is  1
+		if (randomNumber==1) {
+			// -- tell the user whatever they entered is ok.
+			JOptionPane.showMessageDialog(null, "That was ok."); }
+			// 7. If your variable is  2
+if (randomNumber==2) {
+			// -- tell the user whatever they entered is boring.
+JOptionPane.showMessageDialog(null,  "That was very boring."); }
+			// 8. If your variable is  3
+if (randomNumber==3) {
+			// -- invent your own message to give to the user (be nice).
+JOptionPane.showMessageDialog(null,  "Wow. That was truly amazing. Even though I am a computer, unable to feel any emotion, I was blown away by your statement."); }
+		}
+	}
